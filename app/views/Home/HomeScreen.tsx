e1a1340825dfccf13511a3be0b7efeaa9a3e52a7
Motahir <mohommedmotahir.alam@nettantra.net>
2022-09-14 18:53:01 +0530

    update

--- app/views/Home/HomeScreen.tsx
+++ app/views/Home/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import React, {type PropsWithChildren} from 'react';
 import { Text } from 'react-native';
-
-
+import { ConfigStyles } from '../../config';
+import { Config } from '../../config';
 
 export const HomeScreen: React.FC<PropsWithChildren> = (props) => {
 
